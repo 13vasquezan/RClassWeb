1a427759def93d0dae943501329f56adcf397dce
Angelina V <vasquez3.angelina@gmail.com>
2019-04-29 17:14:21 -0400

    tried shiny

--- docs/Tester/app.R
+++ docs/Tester/app.R
@@ -8,26 +8,35 @@
 #
 
 library(shiny)
+library(dplyr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Star Wars data"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+        selectInput("select_species",
+                    label=h3("Select box"),
+                    choices=unique(starwars$species),
+                    multiple= TRUE,
+                    selected=1),
+        
+         sliderInput("height_range",
+                     label = h3("Pick height range"),
+                     min = min(starwars$height,
+                              na.rm=TRUE),
+                    max = max(starwars$height,
+                             na.rm=TRUE),
+                    value = c(100, 150))
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         dataTableOutput("sw")
       )
    )
 )
@@ -35,14 +44,13 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$sw-renderdatatable({
+     starwars[,1:10]%>%
+       filter(height>-input$height_range[1],
+              height<-input$height_range[2],
+              species %in% input$select_species--TRUE)
    })
+  
 }
 
 # Run the application 
